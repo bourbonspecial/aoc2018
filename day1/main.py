ce3f406343893f74cef2d137424f1acbdf33f4d2
Remus Knowles <remknowles@gmail.com>
2018-12-01 09:55:32 +0000

    day 1 part 2

--- day1/main.py
+++ day1/main.py
@@ -1,9 +1,27 @@
 def summer(inp):
 	return sum([int(x) for x in inp.replace('+','').split('\n')])
 
+def counter(inp):
+	s = 0
+	dupe = None
+	clean = [int(x) for x in inp.replace('+','').split('\n')]
+
+	counter = {}
+
+	while not dupe:
+		for i in clean:
+			if s in counter:
+				dupe = s
+				print dupe
+				break
+			else:
+				counter[s] = 0
+
+			s += i
+
 def main():
 	with open('in.txt') as f:
-		print summer(f.read())
+		counter(f.read())
 
 if __name__ == '__main__':
 	main()
