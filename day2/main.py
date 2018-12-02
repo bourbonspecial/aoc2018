cee065dcdb0c8fdaa37fc7aadc7e57dc2d7ecb3d
Remus Knowles <remknowles@gmail.com>
2018-12-02 19:47:30 +0000

    day 2 part 2

--- day2/main.py
+++ day2/main.py
@@ -24,4 +24,28 @@ def checksum(inp):
 
 	return two_count * three_count
 
-print checksum('in_real.txt')
+def sim(w1,w2):
+	"""
+	Calculate the similarity between two words. Assumed to be equal length.
+	"""
+
+	cnt = 0
+	for i, c in enumerate(w1):
+		if c == w2[i]:
+			cnt += 1
+
+	return cnt
+
+# Shit algorithm. O(n^2), can only get away with it because the word list is short.
+with open('in_real.txt') as f:
+	raw = f.read().split('\n')
+
+max_sim = 0
+
+for w1 in raw:
+	for w2 in raw:
+		if not w1 == w2:
+			s = sim(w1,w2)
+			if s >= max_sim:
+				max_sim = s
+				print w1, w2, s
