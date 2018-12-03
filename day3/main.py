cc6eb669d8e497601ec1e90618a9c90503b62fc3
Remus Knowles <remknowles@gmail.com>
2018-12-03 21:59:12 +0000

    day 3 part 2

--- day3/main.py
+++ day3/main.py
@@ -16,10 +16,25 @@ for claim in raw:
 		for j in range(start_b + 1, end_b + 1):
 			grid[i][j] += 1
 
-cnt = 0
-for r in grid:
-	for c in r:
-		if c >= 2:
-			cnt+= 1
+for claim in raw:
+	start_a = int(claim.split()[2].replace(':','').split(',')[0])
+	start_b = int(claim.split()[2].replace(':','').split(',')[1])
 
-print cnt
+	end_a = start_a + int(claim.split()[3].split('x')[0])
+	end_b = start_b + int(claim.split()[3].split('x')[1])
+
+	claim_sum = 0
+	for i in range(start_a + 1, end_a + 1):
+		for j in range(start_b + 1, end_b + 1):
+			claim_sum += grid[i][j]
+
+	if claim_sum == (end_a - start_a) * (end_b - start_b):
+		print claim
+
+# Part 1
+# cnt = 0
+# for r in grid:
+# 	for c in r:
+# 		if c == 1:
+# 			cnt+= 1
+
